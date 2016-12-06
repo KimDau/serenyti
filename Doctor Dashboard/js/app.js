a67f71ad94da09abdcf0b0b3f3acb56f553942c3
Icetoo <florent.enaux@gmail.com>
2016-12-06 07:39:08 +0100

    ajout d'un password dans le cookie, reste quelque modification a faire

--- Doctor Dashboard/js/app.js
+++ Doctor Dashboard/js/app.js
@@ -1,12 +1,14 @@
-var CookieName = ["username","test"];
+var CookieName = ["username","autrenom"]; //The value to make on the cookie (to add on the code)
 var ExpirationTime = 1; //1 day
 
-function setCookie(cname, cvalue, exdays) { //Create cookie
+function setCookie(username, password) { //Create cookie
     var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    d.setTime(d.getTime() + (ExpirationTime*24*60*60*1000));
     var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/"; //Initialiaze cookie value
-}
+    console.log("test = "+username + " ? " + password);
+    document.cookie = CookieName[0] + "=" + username + "-" + CookieName[1] + "=" + password +";"+ expires + ";path=/"; //Initialiaze cookie value
+    console.log(document.cookie);
+    }
 
 function getCookie(cname) { //Permit acces to the cookie
     var name = cname + "=";
@@ -35,10 +37,33 @@ function checkCookie() { //Check cookie value
     }
 }
 
-$(document).ready(function(){
-    $("form").on("submit", function(){
-        alert("You have tried to connect");
-        return false; //Do not return to the index page
-        //return true; //That permit it
+function checkPassword(username,password) { //Check the password, into the cookie (TO MODIFY)
+    var userpassword = getCookie(CookieName[1]);
+    var result = true;
+    //console.log("userpassword : " + userpassword + " vs " + password);
+    if (userpassword == "") 
+       {
+        setCookie(username,password);
+       }
+    else
+       {
+            if(userpassword != password)
+            {
+                result = false;
+            }
+       }
+    return result;
+}
+
+$(document).ready(function(){ //check the document changes
+    $("form").on("submit", function(event){ //On the form submit
+        event.preventDefault();
+        //alert("You have tried to connect");
+        console.log("You have tried to connect");
+        var User = $( this ).serializeArray();
+        checkPassword(User[0].value,User[1].value);
+        console.log("user : " + getCookie(CookieName[0]) +" mot de passe : " + getCookie(CookieName[1]));
+        return false;
+        //return checkPassword(); //Return to the index page if you have the right name
     });
 });
