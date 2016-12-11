00187ced6deaa57cac1606ffb67b8d488578f767
Icetoo <florent.enaux@gmail.com>
2016-12-11 01:38:48 +0100

    ajout de la connexion mongodb avec l'ajout d'un test(a continuer)

--- Doctor_Dashboard/js/request.js
+++ Doctor_Dashboard/js/request.js
@@ -1,11 +1,11 @@
 var PROMvalue = [[]];
 var xhttp = new XMLHttpRequest();
-var SendUrl = "send";
+var SendUrl = "/send";
 var lengthPROM = 4; //The length of the prom survey we wish
 
 function sendPROM()
 {
-	console.log(PROMvalue.length);
+	console.log("données replis = " + PROMvalue.length);
 	if(PROMvalue.length != lengthPROM) //If it's fine
 	{
 		console.log("PROM survey not fulfill");
@@ -13,16 +13,17 @@ function sendPROM()
 	else
 	{
 		var params = "value=";
-		for(var i = 0; i < PROMvalue.length;i++) //check the length and add the prom value
+        params+=PROMvalue[0][0]; //For don't having the "," on the last character
+		for(var i = 1; i < PROMvalue.length;i++) //check the length and add the prom value
 		{
-			params+=PROMvalue[i][0]+",";
+			params+=","+PROMvalue[i][0];
 		}
-		params += "value=";
-		for(var i = 0; i < PROMvalue.length;i++) //check the length
+		params += "&question=";
+        params+=PROMvalue[0][1];//For don't having the "," on the last character
+        for(var i = 1; i < PROMvalue.length;i++) //check the length
 		{
-			params+=PROMvalue[i][1]+",";
+			params+=","+PROMvalue[i][1];
 		}
-		console.log(params);
 		sendData(params);
 	}
 }
@@ -34,11 +35,12 @@ function sendData(params) //send the data of the prom survey. Have a single stri
 
 	xhttp.onload = function() {//Call a function when the state changes.
 	        console.log(this.responseText)
-	}
+	};
 	xhttp.send(params); //Send the data. On the page '/Doctor_Dashboard/send'
 }
 
 function changeValue(nbQuestion, value)
 {
 	PROMvalue[nbQuestion] = [value,nbQuestion];
+	//If possible add the function which darken the button
 }
--- index.js
+++ index.js
@@ -8,45 +8,50 @@ var request = require('request');
 // Connect to the db
 MongoClient.connect("mongodb://localhost:27017/Pillar", function(err, db) {
   if(!err) {
-    console.log("We are connected");
+    console.log("Connected to the Pillar database");
   }
-});
 
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-})); 
+  app.use( bodyParser.json() );       // to support JSON-encoded bodies
+  app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+  }));
 
-//To get the html/css/js on the folder /Doctor_Dashboard and /Page_Pillar
-app.use('/Doctor_Dashboard', express.static(__dirname + '/Doctor_Dashboard'));
-app.use('/Page_Pillar', express.static(__dirname + '/Page_Pillar'));
+  //To get the html/css/js on the folder /Doctor_Dashboard and /Page_Pillar
+  app.use('/Doctor_Dashboard', express.static(__dirname + '/Doctor_Dashboard'));
+  app.use('/Page_Pillar', express.static(__dirname + '/Page_Pillar'));
 
-//To send on the good page. Name can be change as we wish
+  //To send on the good page. Name can be change as we wish
 
-app.get('/pillar',function (req, res) {
-  res.sendFile(path.join(__dirname+'/Page_Pillar/index.html'));
-});
-app.get('/login', function (req, res) {
-  res.sendFile(path.join(__dirname+'/Doctor_Dashboard/login.html'));
-});
-app.get('/dashboard', function (req, res) {
-  res.sendFile(path.join(__dirname+'/Doctor_Dashboard/dashboard.html'));
-});
-app.post('/dashboard', function (req, res) {
-  res.redirect('/dashboard');
-});
-app.get('/tasks', function (req, res) {
-  res.sendFile(path.join(__dirname+'/Doctor_Dashboard/tasks.html'));
-});
+  app.get('/pillar',function (req, res) {
+    res.sendFile(path.join(__dirname+'/Page_Pillar/index.html'));
+  });
+  app.get('/login', function (req, res) {
+    res.sendFile(path.join(__dirname+'/Doctor_Dashboard/login.html'));
+  });
+  app.get('/dashboard', function (req, res) {
+    res.sendFile(path.join(__dirname+'/Doctor_Dashboard/dashboard.html'));
+  });
+  app.post('/dashboard', function (req, res) {
+    res.redirect('/dashboard');
+  });
+  app.get('/tasks', function (req, res) {
+    res.sendFile(path.join(__dirname+'/Doctor_Dashboard/tasks.html'));
+  });
 
-//receiver
-app.post('/send',function (req, res) {
-  var name = req.body.name;
-  console.log(name);
+  //receiver
+  app.post('/send',function (req, res) {
+    var value = req.body.value;
+    var question = req.body.question;
+    db.collection("patient").insertOne({
+      name:"test"
+    });
+  });
+
+
 });
 
 
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Listening on port 3000!')
 });
